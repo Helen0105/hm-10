a391d769fd66f15d97a69e34f2d67d1480f00fc8
Helen0105 <marinenkoolena21@gmail.com>
2026-04-26 12:10:14 +0300

    last  commit

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ setInterval(() => {
   allSquares.forEach((oneSquare) => {
     if (size === true) {
       oneSquare.style.width = "200px";
-      oneSquare.style.height = "150px";
+      // oneSquare.style.height = "150px";
     } else {
       oneSquare.style.width = "100px";
       oneSquare.style.height = "100px";
@@ -44,6 +44,70 @@ setInterval(() => {
 
 
 // let score = 0;
+
+
+
+
+
+
+
+
+
+
+// const target = document.getElementById('circle-target');
+// const scoreTab = document.getElementById('score'); 
+// const area = document.getElementById('game-container');
+
+// let count = 0;
+
+// function move() {
+// const x = Math.floor(Math.random() * (window.innerWidth - 30));
+// const y = Math.floor(Math.random() * (window.innerHeight - 30));
+
+//     target.style.left = x + 'px';
+//     target.style.top = y + 'px';
+// }
+
+
+// setInterval(move, 1000);
+
+
+
+
+
+
+
+
+
+const target = document.getElementById('circle-target');
+const scoreTab = document.getElementById('score'); 
+const area = document.getElementById('game-container');
+
+let count = 0;
+
+function move() {
+  // const x = Math.floor(Math.random() * (window.innerWidth - 30));
+// const y = Math.floor(Math.random() * (window.innerHeight - 30));
+    const x = Math.floor(Math.random() * (area.clientWidth - 30));
+    const y = Math.floor(Math.random() * (area.clientHeight - 30));
+
+    target.style.left = x + 'px';
+    target.style.top = y + 'px';
+}
+
+
+setInterval(move, 1500);
+target.onclick = () => {
+    count++;
+    scoreTab.textContent = count;
+};
+
+
+
+
+
+
+
 
 
 
